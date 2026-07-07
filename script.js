be89b24e44ca3a1e3d49a8843da79bbf2ab6117f
Cristian-Neva <cristianeva72@gmail.com>
2026-07-07 15:42:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const sections=document.querySelectorAll("section");
+
+const mostrar=()=>{
+
+sections.forEach(section=>{
+
+const top=section.getBoundingClientRect().top;
+
+if(top<window.innerHeight-100){
+
+section.style.opacity="1";
+section.style.transform="translateY(0)";
+
+}
+
+});
+
+}
+
+sections.forEach(section=>{
+
+section.style.opacity="0";
+section.style.transform="translateY(40px)";
+section.style.transition="all .8s ease";
+
+});
+
+window.addEventListener("scroll",mostrar);
+
+mostrar();
